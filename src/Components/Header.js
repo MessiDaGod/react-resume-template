b535cefcf38a78f1523a8484040f52c42d00f111
Joe Shakely <joeshakely@gmail.com>
2020-03-06 15:10:46 -0800

    hashrouter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -17,56 +18,58 @@ class Header extends Component {
     }
 
     return (
-      <header id="home">
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
+      <HashRouter>
+        <header id="home">
+          <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+              Show navigation
+            </a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">
+              Hide navigation
+            </a>
 
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
+            <ul id="nav" className="nav">
+              <li className="current">
+                <a className="smoothscroll" href="#home">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#contact">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#resume">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="row banner">
-          <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-            <h3>
-              <span>{occupation}</span>
-            </h3>
-            <hr />
-            <ul className="social">{networks}</ul>
+          <div className="row banner">
+            <div className="banner-text">
+              <h1 className="responsive-headline">{name}</h1>
+              <h3>
+                <span>{occupation}</span>
+              </h3>
+              <hr />
+              <ul className="social">{networks}</ul>
+            </div>
           </div>
-        </div>
 
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
-          </a>
-        </p>
-      </header>
+          <p className="scrolldown">
+            <a className="smoothscroll" href="#about">
+              <i className="icon-down-circle"></i>
+            </a>
+          </p>
+        </header>
+      </HashRouter>
     );
   }
 }
